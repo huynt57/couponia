e346565c62f056aa7d8024cee28c45e4db470a20
Huy Nguyen <huynt57@gmail.com>
2016-09-08 17:50:55 +0700

    update

--- app/Deal/Functions.php
+++ app/Deal/Functions.php
@@ -7,8 +7,18 @@
 namespace App\Deal;
 
 use App\Account;
+use App\Category;
 
 class Functions
 {
-
+    public static function getCategories()
+    {
+        if(!cache()->has('categories'))
+        {
+            $categories = Category::all();
+            cache()->set('categories', $categories);
+            return $categories;
+        }
+        return cache()->get('categories');
+    }
 }
