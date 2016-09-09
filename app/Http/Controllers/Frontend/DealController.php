e3de7b46fcf475fcf541663f3ee9de7701854623
Huy Nguyen <huynt57@gmail.com>
2016-09-09 17:58:47 +0700

    update

--- app/Http/Controllers/Frontend/DealController.php
+++ app/Http/Controllers/Frontend/DealController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Frontend;
 use App\Category;
 use App\Deal;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Deal\Functions;
