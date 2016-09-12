98c6056b712ddf739832d8bf43204262b3d41f0e
Huy Nguyen <huynt57@gmail.com>
2016-09-12 17:51:24 +0700

    update

--- app/Http/Controllers/Frontend/DealController.php
+++ app/Http/Controllers/Frontend/DealController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Category;
 use App\Deal;
+use App\Product;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -73,6 +74,70 @@ class DealController extends Controller
 
         return view('frontend.deal', [
             'deals' => $deal
+        ]);
+    }
+
+    public function getAllProducts()
+    {
+        $products = Product::all()->paginate(config('constants.PAGINATE_NUMBER'));
+
+        return view('frontend.deals', [
+            'deals' => $products
+        ]);
+    }
+
+    public function getProductsByCategory($categoryId)
+    {
+        if(!is_numeric($categoryId))
+        {
+            return 'Category không tồn tại';
+        }
+
+        $checkCategory = Category::find($categoryId)->count();
+
+        if($checkCategory == 0)
+        {
+            return 'Category không tồn tại';
+        }
+
+        $products = Products::where('category_id', $categoryId)->paginate(config('constants.PAGINATE_NUMBER'));
+
+        return view('frontend.deals', [
+            'deals' => $products
+        ]);
+
+    }
+
+
+    public function getProductsBySource($source)
+    {
+
+        $products = Product::where('source', $source)->paginate(config('constants.PAGINATE_NUMBER'));
+
+        return view('frontend.deals', [
+            'deals' => $products
+        ]);
+    }
+
+
+    public function getProductById($id)
+    {
+        if(!is_numeric($id))
+        {
+            return 'Khuyến mãi không tồn tại';
+        }
+
+        $checkProduct = Product::find($id)->count();
+
+        if($checkProduct == 0)
+        {
+            return 'Khuyến mãi không tồn tại';
+        }
+
+        $product = Deal::find($id);
+
+        return view('frontend.deal', [
+            'deals' => $product
         ]);
     }
 
